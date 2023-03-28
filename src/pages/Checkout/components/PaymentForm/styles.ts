f67a96446c54f2d66c2fe7ecd14a4ce1302d3c6f
jnorberto23 <joaovictor.norberto@outlook.com>
2023-03-28 13:26:29 -0300

    🐛 adjust width of buttons ons paymentForm

--- src/pages/Checkout/components/PaymentForm/styles.ts
+++ src/pages/Checkout/components/PaymentForm/styles.ts
@@ -5,7 +5,7 @@ export const PaymentWrapper = styled.div`
   flex-direction: row;
   align-items: flex-start;
   justify-content: flex-start;
-  gap: 1rem;
+  gap: 0.7rem;
 `;
 
 export const InputWrapper = styled.div`  
@@ -15,10 +15,12 @@ export const InputWrapper = styled.div`
   }
 
   label {
+    display: inline-block;
+    width: 11.5rem;
     display: flex;
     align-items: center;
     flex-direction: row;
-    justify-content: space-between;
+    justify-content: flex-start;
     gap: 0.5rem;
     font-size: 0.8rem;
     cursor: pointer;
