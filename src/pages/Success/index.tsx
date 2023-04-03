2be95457313eafa17d79feb1a89382980ef0043b
jnorberto23 <joaovictor.norberto@outlook.com>
2023-04-03 12:37:14 -0300

    🚧work on Success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,16 +1,37 @@
 import { Fragment } from "react";
-import { InfoWrapper, SubTitle, Title, Wrapper } from "./styles";
+import {
+  IconAndTextWrapper,
+  ImageWrapper,
+  InfoImageWrapper,
+  InfoWrapper,
+  SubTitle,
+  TextWrapper,
+  Title,
+  Wrapper,
+} from "./styles";
 
+
+import Illustration from '../../assets/Illustration.png'
+import { MapPin } from "phosphor-react";
 export function Success() {
   return (
     <Fragment>
-        <Wrapper>
+      <Wrapper>
         <Title>Uhu! Pedido confirmado</Title>
-        <SubTitle>Agora é só aguardar que logo o café chegará até você</SubTitle>
+        <SubTitle>
+          Agora é só aguardar que logo o café chegará até você
+        </SubTitle>
 
-        <InfoWrapper>aaaaaaaaa</InfoWrapper>
-        </Wrapper>
-     
+        <InfoImageWrapper>
+          <InfoWrapper>
+            <IconAndTextWrapper color="brand-purple">
+              <MapPin weight="fill"/>
+              <TextWrapper>Entrega em Rua João Daniel Martinelli, 102Farrapos - Porto Alegre, RS</TextWrapper>
+            </IconAndTextWrapper>
+          </InfoWrapper>
+        <ImageWrapper src={Illustration} alt="" />
+        </InfoImageWrapper>
+      </Wrapper>
     </Fragment>
   );
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 
+export type spanVariant = "dark-yellow" | "brand-yellow" | "brand-purple";
+interface SpanProps {
+  color: spanVariant;
+}
+
 export const Wrapper = styled.div`
   padding-top: 2rem;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: flex-start;
+  width: 100%;
 `;
 export const Title = styled.h1`
   color: ${(props) => props.theme["dark-yellow"]};
@@ -24,14 +30,74 @@ export const SubTitle = styled.h1`
   font-size: 1.2rem;
   line-height: 130%;
 `;
+3;
+export const InfoImageWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  margin: auto;
+  padding: auto;
+  gap: 5rem;
+  padding-top: 2rem;
+`;
 
 export const InfoWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  height: 35vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  margin: auto;
+  padding: auto;
+
+  ::before {
+    border-radius: 10px 50px 10px 50px;
+    border: 2px solid transparent; 
+    background: linear-gradient(45deg, yellow, purple) border-box; /*3*/
+    -webkit-mask: /*4*/ linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    -webkit-mask-composite: xor; 
+    mask-composite: exclude; 
+    content: "";
+    inset: 0;
+    position: absolute;
+  }
+`;
+
+export const ImageWrapper = styled.img`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 34vh;
+  margin: auto;
+  padding: auto;
+`;
+
+export const IconAndTextWrapper = styled.div<SpanProps>`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: center;
+  color: black;
+
+  svg {
+    color: ${(props) => props.theme[props.color]};
+    font-size: 1.3rem;
+    margin-right: 0.5rem;
+  }
+`;
+
+export const TextWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: flex-start;
-  justify-content: flex-start;
-  
-  border: 5px solid;
-  border-image: linear-gradient(45deg, purple, orange) 1;
-
+  justify-content: center;
+  gap: 0.4rem;
 `;
